635d032c57034f75e2edf59479aebdbde78c26b5
Pulido Irene <irene.pulido@lom-t-224798-m.local>
2016-06-10 15:46:07 -0700

    Initial Commit

---
+++ HighwayGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HighwayGame
+//
+//  Created by Pulido, Irene on 6/10/16.
+//  Copyright © 2016 Gabriel Pulido. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HighwayGame/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  HighwayGame
+//
+
+//  Copyright (c) 2016 Gabriel Pulido. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
